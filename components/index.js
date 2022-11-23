b5fb5f9844bf5bf35b02a01137980b9a806a7ff4
AdilKhan08 <Adil.noorKhan08@gmail.com>
2022-11-23 20:52:50 +0100

    Setup and basic UI display

---
+++ components/index.js
@@ -0,0 +1,6 @@
+export { default as NavigationBar } from "./NavigationBar/NavigationBar.jsx";
+export { default as MainBanner } from "./MainBanner/MainBanner.jsx";
+export { default as Items } from "./Items/Items.jsx";
+export { default as SecondaryBanner } from "./SecondaryBanner/SecondaryBanner.jsx";
+export { default as CartSection } from "./CartSection/CartSection.jsx";
+export { default as Footer } from "./Footer/Footer.jsx";
